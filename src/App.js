0370efbe99418ba5b9270268344cb42a3b026096
chani017 <sfgdy058@gmail.com>
2022-04-30 11:52:21 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 import './App.css';
 
-function App() {
+const Cube = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <mesh position = {[0,0,0]}>
+          <boxNufferGeometry attach="geometry" />
+          <meshLambertMaterial attach="material" color="#FF5733" />
+        </mesh>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Canvas>
+        <Cube />
+        <OrbitControls />
+        <ambientLight intensity={0.5}/>
+    </Canvas>
+  )
+};
